fb2dc3deaa0c9b85a61e5da2688e5ded3997c35b
Pawan kumar <pawankumarpawankumar619@gmail.com>
2020-11-26 09:57:41 +0530

    Removed server URL

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,5 +1,5 @@
 // connect to socket server
-const socket = io.connect("https://useboard.herokuapp.com/");
+const socket = io.connect();
 // *********************************Basic Setup
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
